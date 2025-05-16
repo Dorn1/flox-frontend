acbe4d26e372d864ff58bfa74f2e1c5e5b95a7a6
Kuba <janochajakub02@gmail.com>
2025-05-16 22:02:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </main>
+</template>
+
+<style>
+main{float: right}
+</style>
