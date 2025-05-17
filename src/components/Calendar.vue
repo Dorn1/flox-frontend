6f96725d5255d3d765c935501df87949325e83c7
Kuba <janochajakub02@gmail.com>
2025-05-17 07:18:25 +0200

    user remembering

---
+++ src/components/Calendar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {watch} from "vue";
+import { useRoute } from 'vue-router';
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const route = useRoute();
+const id = route.params.id;
+let startDay;
+let today = new Date();
+let previousMonday = new Date(today.getTime() - (((today.getDay()+6)%7) * 1000*60*60*24))
+const mondayEvents = ref([]);
+let monday = axios.get("http://localhost:8080/getDay",{params:{userId:id,day:previousMonday.toString()}}).then((res)=>{mondayEvents.value = res.data});
+
+</script>
+
+<template>
+  <main>
+    {{monday}}
+  </main>
+</template>
+
+<style scoped>
+main{
+  color: orange;
+}
+</style>
